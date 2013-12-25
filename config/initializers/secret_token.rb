e2a08995721555ef74d6a8e8e031ae4d3a98cf48
sawyerbowman <sawyerbowman@gmail.com>
2013-12-25 00:45:05 -0500

    Initial Build
    This commit represents the beginning of building my personal website.
    GitHub is recommended as a form of version control. I figured it would
    be good to use throughout the learning process.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SawyerSite::Application.config.secret_key_base = '9ba32bedebd539b6b5316ec68becf116dd1953750f0fad9ffa96d8cc19e99cd304a779a16e06a8d3899905b9b8b8689483093b21e05441f6bd05a4477a35d119'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SawyerSite::Application.config.session_store :cookie_store, key: '_SawyerSite_session'
